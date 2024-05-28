98b174e6a0fc89c83aa322f2ee35202824a57766
MarcAlvarezJ <marcalvarezj@hotmail.com>
2024-05-28 20:30:27 -0300

    Funcionalidad carga de archivo

--- tp_3_Django/urls.py
+++ tp_3_Django/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data_salaries/', include('data_salaries.urls'))
 ]
