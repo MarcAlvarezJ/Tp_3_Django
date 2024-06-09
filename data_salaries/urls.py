2fdcc3f6c4b7c8f26a54ed7e33aacb5960c94b58
Marcodisario <disariomarco06@gmail.com>
2024-06-08 21:31:44 -0300

    filtro y style del fondo
    dropdown de filtro a html, styleo del fondo

--- data_salaries/urls.py
+++ data_salaries/urls.py
@@ -3,7 +3,6 @@ from data_salaries import views
 
 urlpatterns = [
     path('upload/', views.upload, name='upload'),
-    path('success/', views.success, name='success'),
     path('view/', views.view_csv, name='view'),
     path('analisis/', views.analize_data, name='analisis'),
     path('graphs/', views.graphs, name='graphs')
